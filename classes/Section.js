27365a5dddaf40ff0a3d52980e7f698feccf0782
Scott Agrimson <scott.agrimson@gmail.com>
2014-11-25 12:54:08 -0800

    comment out startup shutdown stuff

--- classes/Section.js
+++ classes/Section.js
@@ -38,29 +38,29 @@
             }
         }
 
-        function startup (callbackFn) {
-            if (callbackFn) {
-                callbackFn();
-            } else {
-                if (this.verbose) console.log('Section startup', 'no callbackFn');
-            }
-        }
+        // function startup (callbackFn) {
+        //     if (callbackFn) {
+        //         callbackFn();
+        //     } else {
+        //         if (this.verbose) console.log('Section startup', 'no callbackFn');
+        //     }
+        // }
 
-        function show (callbackFn) {
-            if (callbackFn) {
-                callbackFn();
-            } else {
-                if (this.verbose) console.log('Section show', 'no callbackFn');
-            }
-        }
+        // function show (callbackFn) {
+        //     if (callbackFn) {
+        //         callbackFn();
+        //     } else {
+        //         if (this.verbose) console.log('Section show', 'no callbackFn');
+        //     }
+        // }
 
-        function shutdown (callbackFn) {
-            if (callbackFn) {
-                callbackFn();
-            } else {
-                if (this.verbose) console.log('Section shutdown', 'no callbackFn');
-            }
-        }
+        // function shutdown (callbackFn) {
+        //     if (callbackFn) {
+        //         callbackFn();
+        //     } else {
+        //         if (this.verbose) console.log('Section shutdown', 'no callbackFn');
+        //     }
+        // }
 
         function keyHandler (e) {
         }
@@ -81,9 +81,13 @@
         Section.prototype.resize = resize;
         Section.prototype.keyHandler = keyHandler;
 
-        Section.prototype.startup = startup;
-        Section.prototype.show = show;
-        Section.prototype.shutdown = shutdown;
+        /**
+        * TODO: I think we might want to add startup shutdown show hide, 
+        * and maybe add & remove to section base class and remove it from nav class
+        */
+        // Section.prototype.startup = startup;
+        // Section.prototype.show = show;
+        // Section.prototype.shutdown = shutdown;
 
         Section.prototype.touchStartHandler = touchStart;
         Section.prototype.touchEndHandler = touchEnd;
