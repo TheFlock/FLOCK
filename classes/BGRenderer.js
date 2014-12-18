02b0a7e7991d46b68badef59c0d4f54a6528e5c6
Jonathan Hooker <jonathan@theflock.com>
2014-12-18 12:59:35 -0800

    rearrange image objects

--- classes/BGRenderer.js
+++ classes/BGRenderer.js
@@ -8,154 +8,159 @@
     if (typeof define === 'function' && define.amd) {
         // AMD. Register as an anonymous module.
         define([
-                'jquery',
-                'FLOCK/utils/DeviceDetect',
-                'greensock/TweenLite.min',
-                'greensock/easing/EasePack.min',
-                'greensock/plugins/CSSPlugin.min'
+            'jquery',
+            'FLOCK/utils/DeviceDetect',
+            'greensock/TweenLite.min',
+            'greensock/easing/EasePack.min',
+            'greensock/plugins/CSSPlugin.min'
             ], function ($) {
-            return (root.classes.BGRenderer = factory($));
-        });
-    } else {
-        root.classes.BGRenderer = factory($);
-    }
-}(window.FLOCK = window.FLOCK || {}, function ($) {
-
-    'use strict';
-
-    var BGRenderer = function (containerId) {
-        this.container = containerId;
-        this.initialized = false;
-        this.outer1 = this.outer2 = this.inner1 = this.inner2 = null;
-        this.image1 = this.image2 = null;
-        this.nextContainer = 0;
-        this.verbose = false;
-        this.width = 0;
-        this.height = 0;
-    }
-
-    function init(){
-        if(this.verbose)console.log('BGRenderer | init');
-        this.initialized = true;
-        this.container = document.getElementById(this.container);
-
-        // create two bg containers
-        this.outer1 = addStyles(document.createElement('div'));
-        this.inner1 = addStyles(document.createElement('div'));
-        this.outer1.appendChild(this.inner1);
-        this.container.appendChild(this.outer1);
-
-        this.outer2 = addStyles(document.createElement('div'));
-        this.inner2 = addStyles(document.createElement('div'));
-        this.outer2.appendChild(this.inner2);
-        this.container.appendChild(this.outer2);
-
-        this.outer1.style.overflow = this.outer2.style.overflow = "hidden";
-
-        // call resize via shell
-        FLOCK.app.Shell.resize();
-    }
-
-    function addStyles(elem){
-        elem.style.position = 'absolute';
-        elem.style.top = elem.style.left = '0px';
-        elem.style.width = elem.style.height = '100%';
-        return elem;
-    }
-
-    function changeBg(imageObj, instant, callbackFn){
-        if(!this.initialized)this.init();
-
-        // add new image
-        var newContainer, oldContainer;
-        var newContainerInner, oldContainerInner;
-        if(this.nextContainer == 0){
-            newContainer = this.outer1;
-            newContainerInner = this.inner1;
-            oldContainer = this.outer2;
-            oldContainerInner = this.inner2;
-            this.image1Obj = imageObj;
-            if (imageObj.place) {
-                this.image1 = imageObj.place(this.inner1);
-                this.currentBG = this.image1;
-            }
+                return (root.classes.BGRenderer = factory($));
+            });
         } else {
-            newContainer = this.outer2;
-            newContainerInner = this.inner2;
-            oldContainer = this.outer1;
-            oldContainerInner = this.inner1;
-            this.image2Obj = imageObj;
-            if (imageObj.place) {
-                this.image2 = imageObj.place(this.inner2);
-                this.currentBG = this.image2;
-            }
+            root.classes.BGRenderer = factory($);
         }
-        this.resize();
+    }(window.FLOCK = window.FLOCK || {}, function ($) {
 
-        this.transition(newContainer, oldContainer, instant, callbackFn);
-    }
+        'use strict';
 
-    function transition(newContainer, oldContainer, instant, callbackFn){
-
-        var that = this;
-        var tl = new TimelineLite({onComplete:function(){
-            that.changeComplete(callbackFn);
-        }.bind(this)});
-        tl.pause();
-
-        var t = (instant) ? 0 : (8 / 3);
-
-        // set initial position and scale out new container
-        tl.to(newContainer, 0, { x: this.width + 'px' });
-
-        // animate positions
-        tl.to(oldContainer, t*3.5/8, { x: -this.width + 'px', ease: Expo.easeInOut}, 0);
-        tl.to(newContainer, t*3.5/8, { x: 0 + 'px', ease: Expo.easeInOut}, 0);
-
-        tl.play();
-    }
-
-    function changeComplete(callbackFn){
-        // remove old image
-        if(this.nextContainer == 0){
-            this.inner2.innerHTML = "";
-            this.image2 = null;
-            this.image2Obj = null;
-        } else {
-            this.inner1.innerHTML = "";
-            this.image1 = null;
-            this.image1Obj = null;
+        var BGRenderer = function (containerId) {
+            this.container = containerId;
+            this.initialized = false;
+            this.image1 = {
+                image: null,
+                outer: null,
+                inner: null,
+                obj: null
+            };
+            this.image2 = {
+                image: null,
+                outer: null,
+                inner: null,
+                obj: null
+            };
+            this.nextContainer = 0;
+            this.verbose = false;
+            this.width = 0;
+            this.height = 0;
         }
 
-        // update nextContainer value
-        this.nextContainer = (this.nextContainer+1)%2;
+        function init(){
+            if(this.verbose)console.log('BGRenderer | init');
+            this.initialized = true;
+            this.container = document.getElementById(this.container);
 
-        if(callbackFn)callbackFn();
-    }
+            // create two bg containers
+            this.image1.outer = this.addStyles(document.createElement('div'));
+            this.image1.inner = this.addStyles(document.createElement('div'));
+            this.image1.outer.appendChild(this.image1.inner);
+            this.container.appendChild(this.image1.outer);
 
-    function clear(){
-        this.inner1.innerHTML = "";
-        this.image1 = null;
-        this.image1Obj = null;
-        this.inner2.innerHTML = "";
-        this.image2 = null;
-        this.image2Obj = null;
-    }
+            this.image2.outer = this.addStyles(document.createElement('div'));
+            this.image2.inner = this.addStyles(document.createElement('div'));
+            this.image2.outer.appendChild(this.image2.inner);
+            this.container.appendChild(this.image2.outer);
 
-    function resize(w, h){
+            this.image1.outer.style.overflow = this.image2.outer.style.overflow = "hidden";
 
-        if(!this.inner1)return;
-        if(!w)w = this.container.offsetWidth;
-        if(!h)h = this.container.offsetHeight;
-        this.width = w;
-        this.height = h;
+            // call resize via shell
+            FLOCK.app.Shell.resize();
+        }
 
-        var img1width = this.image1 ? this.image1.offsetWidth : 0,
-            img1height = this.image1 ? this.image1.offsetHeight : 0,
-            img2width = this.image2 ? this.image2.offsetWidth : 0,
-            img2height = this.image2 ? this.image2.offsetHeight : 0;
+        function addStyles(elem){
+            elem.style.position = 'absolute';
+            elem.style.top = elem.style.left = '0px';
+            elem.style.width = elem.style.height = '100%';
+            return elem;
+        }
 
-        var img1Dimensions = {
+        function changeBg(imageObj, instant, callbackFn){
+            if(!this.initialized)this.init();
+
+            // add new image
+            var newImage, oldImage;
+            if(this.nextContainer == 0){
+                newImage = this.image1;
+                oldImage = this.image2;
+                this.image1.obj = imageObj;
+                if (imageObj.place) {
+                    this.image1.image = this.image1.obj.place(this.image1.inner);
+                    this.currentBG = this.image1;
+                }
+            } else {
+                newImage = this.image2;
+                oldImage = this.image1;
+                this.image2.obj = imageObj;
+                if (imageObj.place) {
+                    this.image2.image = this.image2.obj.place(this.image2.inner);
+                    this.currentBG = this.image2;
+                }
+            }
+            this.resize();
+
+            this.transition(newImage, oldImage, instant, callbackFn);
+        }
+
+        function transition(newImage, oldImage, instant, callbackFn){
+
+            var that = this;
+            var tl = new TimelineLite({onComplete:function(){
+                that.changeComplete(callbackFn);
+            }.bind(this)});
+            tl.pause();
+
+            var t = (instant) ? 0 : (8 / 3);
+
+            // set initial position and scale out new container
+            tl.to(newImage.outer, 0, { x: this.width + 'px' });
+
+            // animate positions
+            tl.to(oldImage.outer, t*3.5/8, { x: -this.width + 'px', ease: Expo.easeInOut}, 0);
+            tl.to(newImage.outer, t*3.5/8, { x: 0 + 'px', ease: Expo.easeInOut}, 0);
+
+            tl.play();
+        }
+
+        function changeComplete(callbackFn){
+            // remove old image
+            if(this.nextContainer == 0){
+                this.image2.inner.innerHTML = "";
+                this.image2.image = null;
+                this.image2.obj = null;
+            } else {
+                this.image1.inner.innerHTML = "";
+                this.image1.image = null;
+                this.image1.obj = null;
+            }
+
+            // update nextContainer value
+            this.nextContainer = (this.nextContainer+1)%2;
+
+            if(callbackFn)callbackFn();
+        }
+
+        function clear(){
+            this.image1.inner.innerHTML = "";
+            this.image1.image = null;
+            this.image1.obj = null;
+            this.image2.inner.innerHTML = "";
+            this.image2.image = null;
+            this.image2.obj = null;
+        }
+
+        function resize(w, h){
+
+            if(!this.image1.inner)return;
+            if(!w)w = this.container.offsetWidth;
+            if(!h)h = this.container.offsetHeight;
+            this.width = w;
+            this.height = h;
+
+            var img1width = this.image1.image ? this.image1.image.offsetWidth : 0,
+            img1height = this.image1.image ? this.image1.image.offsetHeight : 0,
+            img2width = this.image2.image ? this.image2.image.offsetWidth : 0,
+            img2height = this.image2.image ? this.image2.image.offsetHeight : 0;
+
+            var img1Dimensions = {
                 w: img1width,
                 h: img1height
             },
@@ -182,25 +187,27 @@
             bg1OffsetTopMax = ((h-bg1AdjustedHeight)+(paddingH/2))-bgOffsetTopMin,
             bg2OffsetTopMax = ((h-bg2AdjustedHeight)+(paddingH/2))-bgOffsetTopMin;
 
-        if(this.image1){
-            this.image1.style.top = (bgOffsetTopMin+(bg1OffsetTopMax*this.image1Obj.v)).toFixed() + 'px';
-            this.image1.style.left = (bgOffsetLeftMin+(bg1OffsetLeftMax*this.image1Obj.h)).toFixed() + 'px';
-            this.image1.style.width = bg1AdjustedWidth+'px';
-            //this.image1.style.height = bg1AdjustedHeight+'px';
+            if(this.image1.image){
+                this.image1.image.style.top = (bgOffsetTopMin+(bg1OffsetTopMax*this.image1.obj.v)).toFixed() + 'px';
+                this.image1.image.style.left = (bgOffsetLeftMin+(bg1OffsetLeftMax*this.image1.obj.h)).toFixed() + 'px';
+                this.image1.image.style.width = bg1AdjustedWidth+'px';
+                //this.image1.style.height = bg1AdjustedHeight+'px';
+            }
+            if(this.image2.image){
+                this.image2.image.style.top = (bgOffsetTopMin+(bg2OffsetTopMax*this.image2.obj.v)).toFixed() + 'px';
+                this.image2.image.style.left = (bgOffsetLeftMin+(bg2OffsetLeftMax*this.image2.obj.h)).toFixed() + 'px';
+                this.image2.image.style.width = bg2AdjustedWidth+'px';
+                //this.image2.style.height = bg2AdjustedHeight+'px';
+            }
         }
-        if(this.image2){
-            this.image2.style.top = (bgOffsetTopMin+(bg2OffsetTopMax*this.image2Obj.v)).toFixed() + 'px';
-            this.image2.style.left = (bgOffsetLeftMin+(bg2OffsetLeftMax*this.image2Obj.h)).toFixed() + 'px';
-            this.image2.style.width = bg2AdjustedWidth+'px';
-            //this.image2.style.height = bg2AdjustedHeight+'px';
-        }
-    }
 
-    BGRenderer.prototype.init = init;
-    BGRenderer.prototype.changeBg = changeBg;
-    BGRenderer.prototype.changeComplete = changeComplete;
-    BGRenderer.prototype.clear = clear;
-    BGRenderer.prototype.resize = resize;
+        BGRenderer.prototype.init = init;
+        BGRenderer.prototype.addStyles = addStyles;
+        BGRenderer.prototype.changeBg = changeBg;
+        BGRenderer.prototype.changeComplete = changeComplete;
+        BGRenderer.prototype.transition = transition;
+        BGRenderer.prototype.clear = clear;
+        BGRenderer.prototype.resize = resize;
 
-    return BGRenderer;
-}));
+        return BGRenderer;
+    }));
