b52f8e6a116a497cd83ab9e45d2f49231b54ad54
Scott Agrimson <scott.agrimson@gmail.com>
2015-03-02 11:34:51 -0800

    add misspelled version of arrayExecutor for backward compatibility

--- utils/ArrayExecuter.js
+++ utils/ArrayExecuter.js
@@ -7,130 +7,30 @@
 
     if (typeof define === 'function' && define.amd) {
         // AMD. Register as an anonymous module.
-        define([], function () {
+        define([
+            'FLOCK/utils/ArrayExecutor'
+        ], function () {
             // Also create a global in case some scripts
             // that are loaded still are looking for
             // a global even when an AMD loader is in use.
-            return (root.utils.ArrayExecutor = factory());
+            return (root.utils.ArrayExecuter = factory());
         });
     } else {
-
-        root.utils.ArrayExecutor = factory();
+        root.utils.ArrayExecuter = factory();
     }
 }(window.FLOCK = window.FLOCK || {}, function () {
 
     /*
     --------------------------------------------------------------------------------------------------------------------
-    arrayExecutor
+    arrayExecuter
+
+    ArrayExecutor had a typo, this should make the corrected spelling backwards compatible
     --------------------------------------------------------------------------------------------------------------------
     */
 
-    var ArrayExecutor = function (scope, id) {
-        this.task_arr = [];
-        this.defaultScope = scope || this;
-        this.id = id || '';
-        this.verbose = false;
-    }
+    console.log('You are including the misspelled version of ArrayExecutor')
 
-    ArrayExecutor.prototype = {
-        //Exectutes an array of functions
-        //If this is called and another array is currently executing, then
-        //the new set of functions will run before finishing the previous set
-        execute: function (arr) {
-            if(this.verbose)console.log("ArrayExecutor | "+this.id+" | execute");
-            this.addNext(arr);
-            this.runStep('');
-        },
-        addNext: function (arr) {
-            if(this.verbose)console.log("ArrayExecutor | "+this.id+" | addNext");
-            if (typeof arr === 'function') {
-                // add single function
-                this.task_arr.unshift({fn: arr, vars: null});
-            } else {
-                // add elements from array
-                arr.reverse();
-
-                for (var i = 0; i < arr.length; i++) {
-                    if (arr[i]) {
-                        this.task_arr.unshift(arr[i]);
-                    }
-                }
-            }
-        },
-        tackOn: function (arr) {
-            if(this.verbose)console.log("ArrayExecutor | "+this.id+" | tackOn");
-            for (var i=0; i<arr.length; i++) {
-                this.task_arr.push(arr[i]);
-            }
-
-            this.runStep('');
-        },
-        runFunctionInScope: function (arr) {
-            var obj = arr[0];
-            var function_name = arr[1];
-            var optionalVars = (arr.length >2)?arr[2]:null;
-
-            if (arr.length >2) {
-                obj[function_name](arr[2]);
-            } else {
-                obj[function_name]();
-            }
-        },
-        runStep: function (args) {
-            if(this.verbose)console.log("ArrayExecutor | "+this.id+" | runStep");
-
-            if (this.task_arr.length == 0)return;
-
-            var step = this.task_arr.shift();
-            var funct = step.fn;
-
-            step.scope = step.scope || this.defaultScope;
-            step.vars = step.vars || [];
-
-            if (typeof step.vars === "string") {
-                step.vars = [step.vars];
-            }
-
-            funct.apply(step.scope, step.vars);
-
-            nullObj(step);
-        },
-        stepComplete: function (args) {
-            if(this.verbose)console.log("ArrayExecutor | "+this.id+" | stepComplete");
-
-            if (this.task_arr.length > 0) {
-                window.requestAnimationFrame(this.runStep.bind(this));
-            }
-
-        },
-        stepComplete_instant: function (args) {
-            if(this.verbose)console.log("ArrayExecutor | "+this.id+" | stepComplete_instant");
-
-            if (this.task_arr.length > 0) {
-                this.runStep();
-            }
-        },
-        clearArrayExecutor: function () {
-            if(this.verbose)console.log("ArrayExecutor | "+this.id+" | clearArrayExecutor");
-            this.task_arr = [];
-        },
-        destroy: function () {
-            for (var i = 0; i < this.task_arr.length; i++) {
-                nullObj(this.task_arr[i]);
-            };
-            this.task_arr = [];
-            this.defaultScope = null;
-        }
-    }
-
-    function nullObj (obj) {
-        for (var prop in obj) {
-            if (obj.hasOwnProperty(prop)) {
-                obj[prop] = null;
-            }
-        }
-        obj = null;
-    }
+    var ArrayExecutor = FLOCK.utils.ArrayExecutor;
 
     return ArrayExecutor;
 }));
